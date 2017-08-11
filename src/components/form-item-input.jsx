4623754c9951f51281278256f37c52b982cc34bc
Jessica Lord <jessica.lord@mongodb.com>
2017-08-11 17:11:52 -0400

    Rename FormItem → FormItemInput

---
+++ src/components/form-item-input.jsx
@@ -0,0 +1,32 @@
+const React = require('react');
+const PropTypes = require('prop-types');
+
+class FormItemInput extends React.Component {
+
+  render() {
+    return (
+      <div className="form-item">
+        <label>
+          <span className="form-item-label">{this.props.label}</span>
+        </label>
+        <input
+          name={this.props.name}
+          placeholder={this.props.placeholder}
+          onChange={this.props.changeHandler}
+          className="form-control"
+          type="text" />
+      </div>
+    );
+  }
+}
+
+FormItemInput.propTypes = {
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  changeHandler: PropTypes.func.isRequired,
+  placeholder: PropTypes.string
+};
+
+FormItemInput.displayName = 'FormItemInput';
+
+module.exports = FormItemInput;
