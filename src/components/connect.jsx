da9070d41eab7d2c12c2b567246a2ea2168e4a42
Jessica Lord <jessica.lord@mongodb.com>
2017-08-11 17:42:02 -0400

    Create ReadPreference component

--- src/components/connect.jsx
+++ src/components/connect.jsx
@@ -1,18 +1,8 @@
 const React = require('react');
-const Actions = require('../actions');
-const FormItemSelect = require('./form-item-select');
 const FormConnectPort = require('./form-connect-port');
-const FormItemInput = require('./form-item-input');
+const FormReadPreferrence = require('./form-read-preferrence');
 
 class Connect extends React.Component {
-
-  onReadPreferrenceChanged(evt) {
-    Actions.onReadPreferrenceChanged(evt.target.value);
-  }
-
-  onReplicaSetNameChanged(evt) {
-    Actions.onReadPreferrenceChanged(evt.target.value);
-  }
 
   render() {
     return (
@@ -25,26 +15,10 @@ class Connect extends React.Component {
             <h2 data-test-id="connect-header">Connect to Host</h2>
           </header>
           <form data-test-id="connect-form">
-              <FormConnectPort />
-              <hr />
-              <div id="read-preference" className="form-group">
-                <FormItemInput
-                  label="Replica Set Name"
-                  name="replica_set_name"
-                  placeholder=""
-                  changeHandler={this.onReplicaSetNameChanged.bind(this)} />
-                <FormItemSelect
-                  label="Read Preference"
-                  name="read_preference"
-                  options={[
-                    {'primary': 'Primary'},
-                    {'primaryPreferred': 'Primary Preferred'},
-                    {'secondary': 'Secondary'},
-                    {'secondaryPreferred': 'Secondary Preferred'},
-                    {'nearest': 'Nearest'}
-                  ]}
-                  changeHandler={this.onReadPreferrenceChanged.bind(this)} />
-            </div>
+            <FormConnectPort />
+            <hr />
+            <FormReadPreferrence />
+            <hr />
           </form>
         </div>
       </div>
