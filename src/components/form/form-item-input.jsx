d31c0a54e1d2797e2d32fc821af890bbd3852db8
Durran Jordan <durran@gmail.com>
2017-09-03 11:54:44 +0200

    COMPASS-1777: Adding validation styles for input

--- src/components/form/form-item-input.jsx
+++ src/components/form/form-item-input.jsx
@@ -1,8 +1,21 @@
 const React = require('react');
+const ReactTooltip = require('react-tooltip');
 const PropTypes = require('prop-types');
 const { shell } = require('electron');
 
 class FormItemInput extends React.Component {
+
+  getClassName() {
+    const className = 'form-item';
+    if (this.props.error) {
+      return `${className} form-item-has-error`;
+    }
+    return className;
+  }
+
+  getErrorId() {
+    return `form-error-tooltip-${this.props.name}`;
+  }
 
   openLink() {
     shell.openExternal(this.props.link);
@@ -16,11 +29,38 @@ class FormItemInput extends React.Component {
     }
   }
 
+  renderError() {
+    if (this.props.error) {
+      return (
+        <i className="fa fa-exclamation-circle" aria-hidden="true" />
+      );
+    }
+  }
+
+  renderErrorTooltip() {
+    if (this.props.error) {
+      return (
+        <ReactTooltip id={this.getErrorId()} />
+      );
+    }
+  }
+
   render() {
+    const tooltipOptions = this.props.error ? {
+      'data-for': this.getErrorId(),
+      'data-effect': 'solid',
+      'data-place': 'bottom',
+      'data-offset': "{'bottom': -2}",
+      'data-tip': this.props.error,
+      'data-type': 'error'
+    } : {};
     return (
-      <div className="form-item">
+      <div className={this.getClassName()}>
         <label>
-          <span className="form-item-label">{this.props.label}</span>
+          <span className="form-item-label">
+            {this.renderError()}
+            {this.props.label}
+          </span>
           {this.renderInfoSprinkle()}
         </label>
         <input
@@ -30,7 +70,9 @@ class FormItemInput extends React.Component {
           onBlur={this.props.blurHandler}
           value={this.props.value}
           className="form-control"
-          type={this.props.type || 'text'} />
+          type={this.props.type || 'text'}
+          {...tooltipOptions} />
+        {this.renderErrorTooltip()}
       </div>
     );
   }
@@ -44,7 +86,8 @@ FormItemInput.propTypes = {
   placeholder: PropTypes.string,
   value: PropTypes.oneOfType([ PropTypes.string, PropTypes.number ]),
   type: PropTypes.string,
-  link: PropTypes.string
+  link: PropTypes.string,
+  error: PropTypes.string
 };
 
 FormItemInput.displayName = 'FormItemInput';
