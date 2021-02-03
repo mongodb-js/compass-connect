bc094460ba69c02c8346dc229db6ecafcf32e054
mcasimir <maurizio.cas@gmail.com>
2021-02-03 16:47:45 +0100

    fix: allow empty port in input

--- src/components/form/port-input.jsx
+++ src/components/form/port-input.jsx
@@ -7,7 +7,14 @@ import FormInput from './form-input';
 class PortInput extends React.PureComponent {
   static displayName = 'PortInput';
 
-  static propTypes = { port: PropTypes.number };
+  static propTypes = {
+    port: PropTypes.number // initial port
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = { port: props.port };
+  }
 
   /**
    * Changes port.
@@ -15,19 +22,24 @@ class PortInput extends React.PureComponent {
    * @param {Object} evt - evt.
    */
   onPortChanged(evt) {
-    Actions.onPortChanged(evt.target.value);
+    this.setState({ port: evt.target.value });
+    Actions.onPortChanged(evt.target.value ? +evt.target.value : 27017);
   }
 
   render() {
+    if (!this.state) {
+      return;
+    }
+
     return (
       <FormInput
         label="Port"
         name="port"
         placeholder="27017"
         changeHandler={this.onPortChanged.bind(this)}
-        value={this.props.port}
+        value={this.state.port}
         type="number"
-        otherInputAttributes={{min: 1, max: 65536}} />
+        otherInputAttributes={{ min: 1, max: 65536 }} />
     );
   }
 }
